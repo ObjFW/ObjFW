f818004c9e6dad11d416fa7521b356543a79a587
Jonathan Schleifer <js@webkeks.org>
2011-01-01 21:19:54 +0100

    Change license.

--- generators/copyright.h
+++ generators/copyright.h
@@ -1,27 +1,40 @@
 /*
- * Copyright (c) 2008 - 2010
+ * Copyright (c) 2008, 2009, 2010, 2011
  *   Jonathan Schleifer <js@webkeks.org>
  *
  * All rights reserved.
  *
  * This file is part of ObjFW. It may be distributed under the terms of the
- * Q Public License 1.0, which can be found in the file LICENSE included in
+ * Q Public License 1.0, which can be found in the file LICENSE.QPL included in
  * the packaging of this file.
+ *
+ * Alternatively, it may be distributed under the terms of the GNU General
+ * Public License, either version 2 or 3, which can be found in the file
+ * LICENSE.GPLv2 or LICENSE.GPLv3 respectively included in the packaging of this
+ * file.
  */
 
 #import "OFString.h"
 
 #define COPYRIGHT \
-    @"/*\n" \
-    @" * Copyright (c) 2008 - 2010\n" \
-    @" *   Jonathan Schleifer <js@webkeks.org>\n" \
-    @" *\n" \
-    @" * All rights reserved.\n" \
-    @" *\n" \
-    @" * This file is part of ObjFW. It may be distributed under the terms " \
-    @"of the\n" \
-    @" * Q Public License 1.0, which can be found in the file LICENSE " \
-    @"included in\n" \
-    @" * the packaging of this file.\n" \
-    @" */\n" \
+    @"/*\n"								      \
+    @" * Copyright (c) 2008, 2009, 2010, 2011\n"			      \
+    @" *   Jonathan Schleifer <js@webkeks.org>\n"			      \
+    @" *\n"								      \
+    @" * All rights reserved.\n"					      \
+    @" *\n"								      \
+    @" * This file is part of ObjFW. It may be distributed under the terms "  \
+    @"of the\n"								      \
+    @" * Q Public License 1.0, which can be found in the file LICENSE.QPL "   \
+    @"included in\n"							      \
+    @" * the packaging of this file.\n"					      \
+    @" *\n"								      \
+    @" * Alternatively, it may be distributed under the terms of the GNU "    \
+    @"General\n"							      \
+    @" * Public License, either version 2 or 3, which can be found in the "   \
+    @"file\n"								      \
+    @" * LICENSE.GPLv2 or LICENSE.GPLv3 respectively included in the "	      \
+    @"packaging of this\n"						      \
+    @" * file.\n"							      \
+    @" */\n"								      \
     @"\n"
