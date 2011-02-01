7fbfc3533cf3396dee7bb25fa5b98a2621fec3b0
Jonathan Schleifer <js@webkeks.org>
2011-02-01 22:41:31 +0100

    Define __STDC_CONSTANT_MACROS if not defined.

--- src/macros.h
+++ src/macros.h
@@ -19,6 +19,9 @@
 #ifndef __STDC_LIMIT_MACROS
 # define __STDC_LIMIT_MACROS
 #endif
+#ifndef __STDC_CONSTANT_MACROS
+# define __STDC_CONSTANT_MACROS
+#endif
 
 #include <stddef.h>
 #include <stdint.h>
