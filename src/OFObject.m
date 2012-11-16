248ef1f16688b4289b84484006b403bf598be56a
Jonathan Schleifer <js@webkeks.org>
2012-11-17 00:07:44 +0100

    Implement forwarding target for Apple/i386.

--- src/OFObject.m
+++ src/OFObject.m
@@ -67,9 +67,11 @@
 # import "threading.h"
 #endif
 
-#if defined(OF_APPLE_RUNTIME) && defined(__x86_64__)
+#ifdef OF_APPLE_RUNTIME
+# if defined(__x86_64__) || defined(__i386__)
 extern id of_forward(id, SEL, ...);
 extern struct stret of_forward_stret(id, SEL, ...);
+# endif
 #endif
 
 struct pre_ivar {
@@ -258,7 +260,7 @@ void _references_to_categories_of_OFObject(void)
 #if defined(OF_OBJFW_RUNTIME)
 	objc_forward_handler = forward_handler;
 #elif defined(OF_APPLE_RUNTIME)
-# ifdef __x86_64__
+# if defined(__x86_64__) || defined(__i386__)
 	objc_setForwardHandler(of_forward, of_forward_stret);
 # endif
 #endif
