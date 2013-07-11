9510a6acb4a5fa5917c998157e72adb46ff49f01
Jonathan Schleifer <js@webkeks.org>
2013-07-11 13:43:55 +0200

    Add tests for -[forwardingTargetForSelector:].

--- tests/ForwardingTests.m
+++ tests/ForwardingTests.m
@@ -16,14 +16,23 @@
 
 #include "config.h"
 
+#include <string.h>
+
 #import "OFString.h"
 #import "OFAutoreleasePool.h"
+
+#import "macros.h"
 
 #import "TestsAppDelegate.h"
 
 static OFString *module = @"Forwarding";
 static size_t forwardings = 0;
 static bool success = false;
+static id target = nil;
+
+struct stret_test {
+	char s[27];
+};
 
 @interface ForwardingTest: OFObject
 @end
@@ -31,6 +40,12 @@ static bool success = false;
 @interface ForwardingTest (Test)
 + (void)test;
 - (void)test;
+- (OFString*)forwardingTargetTest: (OFConstantString*)fmt, ...;
+- (long double)forwardingTargetFPRetTest;
+- (struct stret_test)forwardingTargetStRetTest;
+@end
+
+@interface ForwardingTarget: OFObject
 @end
 
 static void
@@ -67,6 +82,51 @@ test(id self, SEL _cmd)
 
 	return false;
 }
+
+- (id)forwardingTargetForSelector: (SEL)selector
+{
+	if (sel_isEqual(selector, @selector(forwardingTargetTest:)) ||
+	    sel_isEqual(selector, @selector(forwardingTargetFPRetTest)) ||
+	    sel_isEqual(selector, @selector(forwardingTargetStRetTest)))
+		return target;
+
+	return nil;
+}
+@end
+
+@implementation ForwardingTarget
+- (OFString*)forwardingTargetTest: (OFConstantString*)fmt, ...
+{
+	va_list args;
+	OFString *ret;
+
+	OF_ENSURE(self == target);
+
+	va_start(args, fmt);
+	ret = [[[OFString alloc] initWithFormat: fmt
+				      arguments: args] autorelease];
+	va_end(args);
+
+	return ret;
+}
+
+- (long double)forwardingTargetFPRetTest
+{
+	OF_ENSURE(self == target);
+
+	return 12345678.00006103515625;
+}
+
+- (struct stret_test)forwardingTargetStRetTest
+{
+	struct stret_test ret;
+
+	OF_ENSURE(self == target);
+
+	memcpy(ret.s, "abcdefghijklmnopqrstuvwxyz", 27);
+
+	return ret;
+}
 @end
 
 @implementation TestsAppDelegate (ForwardingTests)
@@ -85,6 +145,21 @@ test(id self, SEL _cmd)
 
 	TEST(@"Forwarding a message and adding an instance method",
 	    R([t test]) && success && R([t test]) && forwardings == 1);
+
+#ifdef OF_HAVE_FORWARDING_TARGET_FOR_SELECTOR
+	target = [[[ForwardingTarget alloc] init] autorelease];
+	TEST(@"-[forwardingTargetForSelector:]",
+	   [([t forwardingTargetTest: @"%@ %@ %@ %@ %@ %@ %@ %@ %@", @"1", @"2",
+				     @"3", @"4", @"5", @"6", @"7", @"8", @"9"])
+	   isEqual: @"1 2 3 4 5 6 7 8 9"])
+	TEST(@"-[forwardingTargetForSelector:] with fp return",
+	    [t forwardingTargetFPRetTest] == 12345678.00006103515625)
+# ifdef OF_HAVE_FORWARDING_TARGET_FOR_SELECTOR_STRET
+	TEST(@"-[forwardingTargetForSelector:] with struct return",
+	    !memcmp([t forwardingTargetStRetTest].s,
+	    "abcdefghijklmnopqrstuvwxyz", 27))
+# endif
+#endif
 
 	[pool drain];
 }
