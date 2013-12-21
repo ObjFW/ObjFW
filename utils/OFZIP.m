26cdb5c74ff923afbc815e63871ab21af869929e
Jonathan Schleifer <js@webkeks.org>
2013-12-21 02:10:35 +0100

    OFZIP: Add quiet mode.

--- utils/OFZIP.m
+++ utils/OFZIP.m
@@ -31,11 +31,13 @@
 #define BUFFER_SIZE 4096
 
 @interface OFZIP: OFObject
-- (void)listFilesInArchive: (OFZIPArchive*)archive
-		   verbose: (bool)verbose;
+{
+	int_fast8_t _override, _outputLevel;
+}
+
+- (void)listFilesInArchive: (OFZIPArchive*)archive;
 - (void)extractFiles: (OFArray*)files
-	 fromArchive: (OFZIPArchive*)archive
-	    override: (int_fast8_t)override;
+	 fromArchive: (OFZIPArchive*)archive;
 @end
 
 OF_APPLICATION_DELEGATE(OFZIP)
@@ -48,13 +50,15 @@ help(OFStream *stream, bool full, int status)
 	    [OFApplication programName]];
 
 	if (full) {
-		[stream writeString: @"\nOptions:\n"
-				     @"    -f  Force / override files\n"
-				     @"    -h  Show this help\n"
-				     @"    -l  List all files in the archive\n"
-				     @"    -n  Never override files\n"
-				     @"    -v  Verbose output for file list\n"
-				     @"    -x  Extract files\n"];
+		[stream writeString:
+		    @"\nOptions:\n"
+		    @"    -f  Force / override files\n"
+		    @"    -h  Show this help\n"
+		    @"    -l  List all files in the archive\n"
+		    @"    -n  Never override files\n"
+		    @"    -q  Quiet mode (no output, except errors)\n"
+		    @"    -v  Verbose output for file list\n"
+		    @"    -x  Extract files\n"];
 	}
 
 	[OFApplication terminateWithStatus: status];
@@ -64,10 +68,8 @@ help(OFStream *stream, bool full, int status)
 - (void)applicationDidFinishLaunching
 {
 	OFOptionsParser *optionsParser =
-	    [OFOptionsParser parserWithOptions: @"fhlnvx"];
+	    [OFOptionsParser parserWithOptions: @"fhlnqvx"];
 	of_unichar_t option, mode = '\0';
-	int_fast8_t override = 0;
-	bool verbose = false;
 	OFArray *remainingArguments;
 	OFZIPArchive *archive;
 	OFArray *files;
@@ -75,13 +77,16 @@ help(OFStream *stream, bool full, int status)
 	while ((option = [optionsParser nextOption]) != '\0') {
 		switch (option) {
 		case 'f':
-			override = 1;
+			_override = 1;
 			break;
 		case 'n':
-			override = -1;
+			_override = -1;
 			break;
 		case 'v':
-			verbose = true;
+			_outputLevel = 1;
+			break;
+		case 'q':
+			_outputLevel = -1;
 			break;
 		case 'l':
 		case 'x':
@@ -111,8 +116,7 @@ help(OFStream *stream, bool full, int status)
 		archive = [OFZIPArchive archiveWithPath:
 		    [remainingArguments firstObject]];
 
-		[self listFilesInArchive: archive
-				 verbose: verbose];
+		[self listFilesInArchive: archive];
 		break;
 	case 'x':
 		if ([remainingArguments count] < 1)
@@ -124,8 +128,7 @@ help(OFStream *stream, bool full, int status)
 		    [remainingArguments firstObject]];
 
 		[self extractFiles: files
-		       fromArchive: archive
-			  override: override];
+		       fromArchive: archive];
 		break;
 	default:
 		help(of_stderr, true, 1);
@@ -136,7 +139,6 @@ help(OFStream *stream, bool full, int status)
 }
 
 - (void)listFilesInArchive: (OFZIPArchive*)archive
-		   verbose: (bool)verbose
 {
 	OFEnumerator *enumerator = [[archive entries] objectEnumerator];
 	OFZIPArchiveEntry *entry;
@@ -144,7 +146,7 @@ help(OFStream *stream, bool full, int status)
 	while ((entry = [enumerator nextObject]) != nil) {
 		void *pool = objc_autoreleasePoolPush();
 
-		if (verbose) {
+		if (_outputLevel > 0) {
 			OFString *date = [[entry modificationDate]
 			    localDateStringWithFormat: @"%Y-%m-%d %H:%M:%S"];
 
@@ -162,7 +164,6 @@ help(OFStream *stream, bool full, int status)
 
 - (void)extractFiles: (OFArray*)files
 	 fromArchive: (OFZIPArchive*)archive
-	    override: (int_fast8_t)override
 {
 	OFEnumerator *enumerator = [[archive entries] objectEnumerator];
 	OFZIPArchiveEntry *entry;
@@ -192,7 +193,7 @@ help(OFStream *stream, bool full, int status)
 		if ([outFileName hasPrefix: @"/"] ||
 		    [outFileName containsString: @":"]) {
 #endif
-			[of_stdout writeFormat: @"Refusing to extract %@!\n",
+			[of_stderr writeFormat: @"Refusing to extract %@!\n",
 						fileName];
 			[OFApplication terminateWithStatus: 1];
 		}
@@ -201,18 +202,20 @@ help(OFStream *stream, bool full, int status)
 		    [[outFileName pathComponents] objectEnumerator];
 		while ((component = [componentEnumerator nextObject]) != nil) {
 			if ([component isEqual: OF_PATH_PARENT_DIRECTORY]) {
-				[of_stdout writeFormat:
+				[of_stderr writeFormat:
 				    @"Refusing to extract %@!\n", fileName];
 				[OFApplication terminateWithStatus: 1];
 			}
 		}
 
-		[of_stdout writeFormat: @"Extracting %@...", fileName];
+		if (_outputLevel > -1)
+			[of_stdout writeFormat: @"Extracting %@...", fileName];
 
 		if ([fileName hasSuffix: @"/"]) {
 			[OFFile createDirectoryAtPath: outFileName
 					createParents: true];
-			[of_stdout writeLine: @" done"];
+			if (_outputLevel > -1)
+				[of_stdout writeLine: @" done"];
 			continue;
 		}
 
@@ -221,11 +224,12 @@ help(OFStream *stream, bool full, int status)
 			[OFFile createDirectoryAtPath: directory
 					createParents: true];
 
-		if ([OFFile fileExistsAtPath: outFileName] && override != 1) {
+		if ([OFFile fileExistsAtPath: outFileName] && _override != 1) {
 			OFString *line;
 
-			if (override == -1) {
-				[of_stdout writeLine: @" skipped"];
+			if (_override == -1) {
+				if (_outputLevel > -1)
+					[of_stdout writeLine: @" skipped"];
 				continue;
 			}
 
@@ -245,9 +249,9 @@ help(OFStream *stream, bool full, int status)
 			    ![line isEqual: @"A"]);
 
 			if ([line isEqual: @"A"])
-				override = 1;
+				_override = 1;
 			else if ([line isEqual: @"N"])
-				override = -1;
+				_override = -1;
 
 			if ([line isEqual: @"n"] || [line isEqual: @"N"]) {
 				[of_stdout writeFormat: @"Skipping %@...\n",
@@ -272,7 +276,7 @@ help(OFStream *stream, bool full, int status)
 			newPercent = (written == size
 			    ? 100 : (int_fast8_t)(written * 100 / size));
 
-			if (percent != newPercent) {
+			if (_outputLevel > -1 && percent != newPercent) {
 				percent = newPercent;
 
 				[of_stdout writeFormat:
@@ -281,7 +285,9 @@ help(OFStream *stream, bool full, int status)
 			}
 		}
 
-		[of_stdout writeFormat: @"\rExtracting %@... done\n", fileName];
+		if (_outputLevel > -1)
+			[of_stdout writeFormat: @"\rExtracting %@... done\n",
+						fileName];
 
 		objc_autoreleasePoolPop(pool);
 	}
