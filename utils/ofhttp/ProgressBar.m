d1792261f0e5ad93e739d658045c55cf66065966
Jonathan Schleifer <js@webkeks.org>
2015-08-22 13:38:54 +0200

    utils/ofhttp: Add includes required on Solaris

--- utils/ofhttp/ProgressBar.m
+++ utils/ofhttp/ProgressBar.m
@@ -18,8 +18,13 @@
 
 #include <math.h>
 
+#include <unistd.h>
+
 #ifdef HAVE_SYS_IOCTL_H
 # include <sys/ioctl.h>
+#endif
+#ifdef HAVE_SYS_TERMIOS_H
+# include <sys/termios.h>
 #endif
 
 #import "OFDate.h"
