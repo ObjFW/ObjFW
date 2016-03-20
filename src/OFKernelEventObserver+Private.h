ObjFW/ObjFW

commit 88f2f032dc65a46ec7a9a8b47b39f4abfc4ddf28

More OFKernelEventObserver refactoring
This was necessary because select(), poll() and kevent() on FreeBSD and
OS X would sometimes return 0 events, even if there are some, and
sometimes return the correct number of events that were pending, meaning
the number of events returned is unreliable. To make things worse,
whether it returns 0 or the number of events that were pending is
completely non-deterministic on both FreeBSD and OS X (running the same
tests multiple times in a row would make it sometimes work and sometimes
fail).

In order to prevent code from depending on the return value of
-[observeForTimeInterval:] (which would depend on select(), poll() and
kevent() returning the correct number), OFKernelObserver no longer
returns whether there were pending events. It is expected that
-[observe] or -[observeForTimeInterval:] is just called in a loop as
long as events should be handled.

The tests have been changed as well to reflect this. What they do now is
set a deadline and call -[observeForTimeInterval:] with a small timeout
in a loop until the deadline is reached or all events have been handled.

Note: DragonFlyBSD has not been tested, but will most likely behave like
FreeBSD and OS X.

src/OFKernelEventObserver+Private.h
@@ -22,5 +22,5 @@
 - (void)OF_removeObjectForReading: (id)object;
 - (void)OF_removeObjectForWriting: (id)object;
 - (void)OF_processQueueAndStoreRemovedIn: (OFMutableArray*)removed;
-- (bool)OF_processReadBuffers;
+- (void)OF_processReadBuffers;
 @end

src/OFKernelEventObserver.m
@@ -340,14 +340,14 @@ enum {
 	[self observeForTimeInterval: -1];
 }
 
-- (bool)observeForTimeInterval: (of_time_interval_t)timeInterval
+- (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
 	OF_UNRECOGNIZED_SELECTOR
 }
 
-- (bool)observeUntilDate: (OFDate*)date
+- (void)observeUntilDate: (OFDate*)date
 {
-	return [self observeForTimeInterval: [date timeIntervalSinceNow]];
+	[self observeForTimeInterval: [date timeIntervalSinceNow]];
 }
 
 - (void)cancel
@@ -360,35 +360,22 @@ enum {
 #endif
 }
 
-- (bool)OF_processReadBuffers
+- (void)OF_processReadBuffers
 {
 	id const *objects = [_readObjects objects];
 	size_t i, count = [_readObjects count];
-	bool foundInReadBuffer = false;
 
 	for (i = 0; i < count; i++) {
 		void *pool = objc_autoreleasePoolPush();
 
 		if ([objects[i] isKindOfClass: [OFStream class]] &&
 		    [objects[i] hasDataInReadBuffer] &&
-		    ![objects[i] OF_isWaitingForDelimiter]) {
-			if ([_delegate respondsToSelector:
-			    @selector(objectIsReadyForReading:)])
-				[_delegate objectIsReadyForReading: objects[i]];
-
-			foundInReadBuffer = true;
-		}
+		    ![objects[i] OF_isWaitingForDelimiter] &&
+		    [_delegate respondsToSelector:
+		    @selector(objectIsReadyForReading:)])
+			[_delegate objectIsReadyForReading: objects[i]];
 
 		objc_autoreleasePoolPop(pool);
 	}
-
-	/*
-	 * As long as we have data in the read buffer for any stream, we don't
-	 * want to block.
-	 */
-	if (foundInReadBuffer)
-		return true;
-
-	return false;
 }
 @end

src/OFKernelEventObserver_epoll.m
@@ -184,19 +184,15 @@ static const of_map_table_functions_t mapFunctions = { NULL };
 		       events: EPOLLOUT];
 }
 
-- (bool)observeForTimeInterval: (of_time_interval_t)timeInterval
+- (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
 	OFNull *nullObject = [OFNull null];
 	void *pool = objc_autoreleasePoolPush();
 	struct epoll_event eventList[EVENTLIST_SIZE];
-	int i, events, realEvents = 0;
+	int i, events;
 
 	[self OF_processQueueAndStoreRemovedIn: nil];
-
-	if ([self OF_processReadBuffers]) {
-		objc_autoreleasePoolPop(pool);
-		return true;
-	}
+	[self OF_processReadBuffers];
 
 	objc_autoreleasePoolPop(pool);
 
@@ -206,9 +202,6 @@ static const of_map_table_functions_t mapFunctions = { NULL };
 	if (events < 0)
 		return [OFObserveFailedException exceptionWithObserver: self
 								 errNo: errno];
-
-	if (events == 0)
-		return false;
 
 	for (i = 0; i < events; i++) {
 		if (eventList[i].data.ptr == nullObject) {
@@ -228,8 +221,6 @@ static const of_map_table_functions_t mapFunctions = { NULL };
 				[_delegate objectIsReadyForReading:
 				    eventList[i].data.ptr];
 
-			realEvents++;
-
 			objc_autoreleasePoolPop(pool);
 		}
 
@@ -241,17 +232,10 @@ static const of_map_table_functions_t mapFunctions = { NULL };
 				[_delegate objectIsReadyForWriting:
 				    eventList[i].data.ptr];
 
-			realEvents++;
-
 			objc_autoreleasePoolPop(pool);
 		}
 
 		assert((eventList[i].events & ~(EPOLLIN | EPOLLOUT)) == 0);
 	}
-
-	if (realEvents == 0)
-		return false;
-
-	return true;
 }
 @end

src/OFKernelEventObserver_kqueue.m
@@ -148,12 +148,12 @@
 	[_changeList addItem: &event];
 }
 
-- (bool)observeForTimeInterval: (of_time_interval_t)timeInterval
+- (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
 	void *pool = objc_autoreleasePoolPush();
 	struct timespec timeout;
 	struct kevent eventList[EVENTLIST_SIZE];
-	int i, events, realEvents = 0;
+	int i, events;
 
 	timeout.tv_sec = (time_t)timeInterval;
 	timeout.tv_nsec = lrint((timeInterval - timeout.tv_sec) * 1000000000);
@@ -164,10 +164,7 @@
 	 */
 	[self OF_processQueueAndStoreRemovedIn: _removedArray];
 
-	if ([self OF_processReadBuffers]) {
-		objc_autoreleasePoolPop(pool);
-		return true;
-	}
+	[self OF_processReadBuffers];
 
 	objc_autoreleasePoolPop(pool);
 
@@ -176,14 +173,11 @@
 	    (timeInterval != -1 ? &timeout : NULL));
 
 	if (events < 0)
-		return [OFObserveFailedException exceptionWithObserver: self
+		@throw [OFObserveFailedException exceptionWithObserver: self
 								 errNo: errno];
 
 	[_changeList removeAllItems];
 	[_removedArray removeAllObjects];
-
-	if (events == 0)
-		return false;
 
 	for (i = 0; i < events; i++) {
 		if (eventList[i].flags & EV_ERROR)
@@ -220,13 +214,6 @@
 		}
 
 		objc_autoreleasePoolPop(pool);
-
-		realEvents++;
 	}
-
-	if (realEvents == 0)
-		return false;
-
-	return true;
 }
 @end

src/OFKernelEventObserver_poll.m
@@ -152,19 +152,15 @@
 		       events: POLLOUT];
 }
 
-- (bool)observeForTimeInterval: (of_time_interval_t)timeInterval
+- (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
 	void *pool = objc_autoreleasePoolPush();
 	struct pollfd *FDs;
 	int events;
-	size_t i, nFDs, realEvents = 0;
+	size_t i, nFDs;
 
 	[self OF_processQueueAndStoreRemovedIn: nil];
-
-	if ([self OF_processReadBuffers]) {
-		objc_autoreleasePoolPop(pool);
-		return true;
-	}
+	[self OF_processReadBuffers];
 
 	objc_autoreleasePoolPop(pool);
 
@@ -182,9 +178,6 @@
 	if (events < 0)
 		@throw [OFObserveFailedException exceptionWithObserver: self
 								 errNo: errno];
-
-	if (events == 0)
-		return false;
 
 	for (i = 0; i < nFDs; i++) {
 		if (FDs[i].fd > _maxFD)
@@ -208,8 +201,6 @@
 				    _FDToObject[FDs[i].fd]];
 
 			objc_autoreleasePoolPop(pool);
-
-			realEvents++;
 		}
 
 		if (FDs[i].revents & POLLOUT) {
@@ -221,16 +212,9 @@
 				    _FDToObject[FDs[i].fd]];
 
 			objc_autoreleasePoolPop(pool);
-
-			realEvents++;
 		}
 
 		FDs[i].revents = 0;
 	}
-
-	if (realEvents == 0)
-		return false;
-
-	return true;
 }
 @end

src/OFKernelEventObserver_select.m
@@ -119,7 +119,7 @@
 	FD_CLR(fd, &_writeFDs);
 }
 
-- (bool)observeForTimeInterval: (of_time_interval_t)timeInterval
+- (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
 	void *pool = objc_autoreleasePoolPush();
 	id const *objects;
@@ -127,14 +127,10 @@
 	fd_set writeFDs;
 	struct timeval timeout;
 	int events;
-	size_t i, count, realEvents = 0;
+	size_t i, count;
 
 	[self OF_processQueueAndStoreRemovedIn: nil];
-
-	if ([self OF_processReadBuffers]) {
-		objc_autoreleasePoolPop(pool);
-		return true;
-	}
+	[self OF_processReadBuffers];
 
 	objc_autoreleasePoolPop(pool);
 
@@ -166,9 +162,6 @@
 		@throw [OFObserveFailedException exceptionWithObserver: self
 								 errNo: errno];
 
-	if (events == 0)
-		return false;
-
 	if (FD_ISSET(_cancelFD[0], &readFDs)) {
 		char buffer;
 
@@ -188,13 +181,9 @@
 
 		pool = objc_autoreleasePoolPush();
 
-		if (FD_ISSET(fd, &readFDs)) {
-			if ([_delegate respondsToSelector:
-			    @selector(objectIsReadyForReading:)])
-				[_delegate objectIsReadyForReading: objects[i]];
-
-			realEvents++;
-		}
+		if (FD_ISSET(fd, &readFDs) && [_delegate respondsToSelector:
+		    @selector(objectIsReadyForReading:)])
+			[_delegate objectIsReadyForReading: objects[i]];
 
 		objc_autoreleasePoolPop(pool);
 	}
@@ -207,20 +196,11 @@
 
 		pool = objc_autoreleasePoolPush();
 
-		if (FD_ISSET(fd, &writeFDs)) {
-			if ([_delegate respondsToSelector:
-			    @selector(objectIsReadyForWriting:)])
-				[_delegate objectIsReadyForWriting: objects[i]];
-
-			realEvents++;
-		}
+		if (FD_ISSET(fd, &writeFDs) && [_delegate respondsToSelector:
+		    @selector(objectIsReadyForWriting:)])
+			[_delegate objectIsReadyForWriting: objects[i]];
 
 		objc_autoreleasePoolPop(pool);
 	}
-
-	if (realEvents == 0)
-		return false;
-
-	return true;
 }
 @end

src/OFKernelEventObserver.h
@@ -202,18 +202,16 @@
  *	  timeout is reached.
  *
  * @param timeInterval The time to wait for an event, in seconds
- * @return A boolean whether events occurred before returning
  */
-- (bool)observeForTimeInterval: (of_time_interval_t)timeInterval;
+- (void)observeForTimeInterval: (of_time_interval_t)timeInterval;
 
 /*!
  * @brief Observes all objects until an event happens on an object or the
  *	  specified date is reached.
  *
  * @param date The until which to observe
- * @return A boolean whether events occurred before returning
  */
-- (bool)observeUntilDate: (OFDate*)date;
+- (void)observeUntilDate: (OFDate*)date;
 
 /*!
  * @brief Cancels the currently blocking observe call.

tests/OFKernelEventObserverTests.m
@@ -18,6 +18,7 @@
 
 #import "OFKernelEventObserver.h"
 #import "OFString.h"
+#import "OFDate.h"
 #import "OFTCPSocket.h"
 #import "OFAutoreleasePool.h"
 
@@ -36,40 +37,156 @@
 
 #import "TestsAppDelegate.h"
 
-static OFString *module;
-static OFKernelEventObserver *observer;
-static int events;
-static id expectedObject;
-static bool readData, expectEOS;
-static OFTCPSocket *accepted;
+#define EXPECTED_EVENTS 3
 
-@interface ObserverDelegate: OFObject
-- (void)objectIsReadyForReading: (id)object;
+static OFString *module;
+
+@interface ObserverTest: OFObject
+{
+@public
+	TestsAppDelegate *_testsAppDelegate;
+	OFKernelEventObserver *_observer;
+	OFTCPSocket *_server, *_client, *_accepted;
+	size_t _events;
+}
+
+- (void)run;
 @end
 
-@implementation ObserverDelegate
+@implementation ObserverTest
+- initWithTestsAppDelegate: (TestsAppDelegate*)testsAppDelegate
+{
+	self = [super init];
+
+	@try {
+		uint16_t port;
+
+		_testsAppDelegate = testsAppDelegate;
+
+		_server = [[OFTCPSocket alloc] init];
+		port = [_server bindToHost: @"127.0.0.1"
+				      port: 0];
+		[_server listen];
+
+		_client = [[OFTCPSocket alloc] init];
+		[_client connectToHost: @"127.0.0.1"
+				  port: port];
+
+		[_client writeBuffer: "0"
+			      length: 1];
+	} @catch (id e) {
+		[self release];
+		@throw e;
+	}
+
+	return self;
+}
+
+- (void)dealloc
+{
+	[_server release];
+	[_client release];
+	[_accepted release];
+
+	[super dealloc];
+}
+
+- (void)run
+{
+	OFDate *deadline;
+	bool deadlineExceeded = false;
+
+	[_testsAppDelegate outputTesting: @"-[observe] with listening socket"
+				inModule: module];
+
+	deadline = [OFDate dateWithTimeIntervalSinceNow: 1];
+	while (_events < EXPECTED_EVENTS) {
+		if ([deadline timeIntervalSinceNow] < 0) {
+			deadlineExceeded = true;
+			break;
+		}
+
+		[_observer observeForTimeInterval: 0.01];
+	}
+
+	if (!deadlineExceeded)
+		[_testsAppDelegate
+		    outputSuccess: @"-[observe] not exceeding deadline"
+			 inModule: module];
+	else
+		[_testsAppDelegate
+		    outputFailure: @"-[observe] not exceeding deadline"
+			 inModule: module];
+
+	if (_events == EXPECTED_EVENTS)
+		[_testsAppDelegate
+		    outputSuccess: @"-[observe] handling all events"
+			 inModule: module];
+	else
+		[_testsAppDelegate
+		    outputFailure: @"-[observe] handling all events"
+			 inModule: module];
+}
+
 - (void)objectIsReadyForReading: (id)object
 {
-	events++;
+	char buf;
 
-	OF_ENSURE(object == expectedObject);
+	switch (_events++) {
+	case 0:
+		if (object == _server)
+			[_testsAppDelegate
+			    outputSuccess: @"-[observe] with listening socket"
+				 inModule: module];
+		else
+			[_testsAppDelegate
+			    outputFailure: @"-[observe] with listening socket"
+				 inModule: module];
 
-	if ([object isListening]) {
-		accepted = [[object accept] retain];
+		_accepted = [[object accept] retain];
+		[_observer addObjectForReading: _accepted];
 
-		[accepted writeBuffer: "0"
-			       length: 1];
-	} else if (readData) {
-		char buf;
+		[_testsAppDelegate
+		    outputTesting: @"-[observe] with data to read available"
+			 inModule: module];
 
-		if (expectEOS)
-			OF_ENSURE([object readIntoBuffer: &buf
-						  length: 1] == 0);
-		else {
-			OF_ENSURE([object readIntoBuffer: &buf
-						  length: 1] == 1);
-			OF_ENSURE(buf == '0');
-		}
+		break;
+	case 1:
+		if (object == _accepted &&
+		    [object readIntoBuffer: &buf
+				    length: 1] == 1 && buf == '0')
+			[_testsAppDelegate
+			    outputSuccess: @"-[observe] with data to read "
+					   @"available"
+				 inModule: module];
+		else
+			[_testsAppDelegate
+			    outputFailure: @"-[observe] with data to read "
+					   @"available"
+				 inModule: module];
+
+		[_client close];
+
+		[_testsAppDelegate
+		    outputTesting: @"-[observe] with closed connection"
+			 inModule: module];
+
+		break;
+	case 2:
+		if (object == _accepted &&
+		    [object readIntoBuffer: &buf
+				    length: 1] == 0)
+			[_testsAppDelegate
+			    outputSuccess: @"-[observe] with closed connection"
+				 inModule: module];
+		else
+			[_testsAppDelegate
+			    outputFailure: @"-[observe] with closed connection"
+				 inModule: module];
+
+		break;
+	default:
+		OF_ENSURE(0);
 	}
 }
 @end
@@ -77,53 +194,20 @@ static OFTCPSocket *accepted;
 @implementation TestsAppDelegate (OFKernelEventObserverTests)
 - (void)kernelEventObserverTestsWithClass: (Class)class
 {
-	ObserverDelegate *delegate =
-	    [[[ObserverDelegate alloc] init] autorelease];
-	OFTCPSocket *sock1 = [OFTCPSocket socket];
-	OFTCPSocket *sock2 = [OFTCPSocket socket];
-	uint16_t port;
+	ObserverTest *test;
 
 	module = [class className];
-	events = 0;
-	expectedObject = nil;
-	readData = expectEOS = false;
-	accepted = nil;
-
-	port = [sock1 bindToHost: @"127.0.0.1"
-			    port: 0];
-	[sock1 listen];
+	test = [[[ObserverTest alloc]
+	    initWithTestsAppDelegate: self] autorelease];
 
 	TEST(@"+[observer]",
-	    (observer = [class observer]) &&
-	    R([observer setDelegate: delegate]))
+	    (test->_observer = [OFKernelEventObserver observer]))
+	[test->_observer setDelegate: test];
 
 	TEST(@"-[addObjectForReading:]",
-	    R([observer addObjectForReading: sock1]))
+	    R([test->_observer addObjectForReading: test->_server]))
 
-	[sock2 connectToHost: @"127.0.0.1"
-			port: port];
-	TEST(@"-[observe] waiting for connection",
-	    (expectedObject = sock1) &&
-	    [observer observeForTimeInterval: 0.01])
-	[accepted autorelease];
-
-	TEST(@"-[observe] waiting for data",
-	    (expectedObject = sock2) &&
-	    R([observer addObjectForReading: sock2]) &&
-	    [observer observeForTimeInterval: 0.01])
-
-	TEST(@"-[observe] keeping event until read",
-	    R(readData = true) && [observer observeForTimeInterval: 0.01])
-
-	TEST(@"-[observe] time out due to no events",
-	    R(readData = false) && ![observer observeForTimeInterval: 0.01])
-
-	[accepted close];
-	TEST(@"-[observe] closed connection",
-	    R(readData = true) && R(expectEOS = true) &&
-	    [observer observeForTimeInterval: 0.01])
-
-	TEST(@"-[observe] correct number of events", events == 4)
+	[test run];
 }
 
 - (void)kernelEventObserverTests

commit 7d55cd219635ee2d5d7abe8711ba5129e00ce396

Never block when the read buffer is non-empty
This was broken by 88f2f03. The problem only existed when something was
in the read buffer, but not processed completely, as after processing
the read buffer, it would go on to wait for data - but since not the
entire read buffer had been processed, it meant there was still data
left there that needed to be handled first.

src/OFKernelEventObserver+Private.h
@@ -19,7 +19,7 @@
 OF_ASSUME_NONNULL_BEGIN
 
 @interface OFKernelEventObserver ()
-- (void)OF_processReadBuffers;
+- (bool)OF_processReadBuffers;
 @end
 
 OF_ASSUME_NONNULL_END

src/OFKernelEventObserver.m
@@ -215,19 +215,30 @@
 #endif
 }
 
-- (void)OF_processReadBuffers
+- (bool)OF_processReadBuffers
 {
+	bool foundInReadBuffer = false;
+
 	for (id object in _readObjects) {
 		void *pool = objc_autoreleasePoolPush();
 
 		if ([object isKindOfClass: [OFStream class]] &&
 		    [object hasDataInReadBuffer] &&
-		    ![object OF_isWaitingForDelimiter] &&
-		    [_delegate respondsToSelector:
-		    @selector(objectIsReadyForReading:)])
-			[_delegate objectIsReadyForReading: object];
+		    ![object OF_isWaitingForDelimiter]) {
+			if ([_delegate respondsToSelector:
+			    @selector(objectIsReadyForReading:)])
+				[_delegate objectIsReadyForReading: object];
+
+			foundInReadBuffer = true;
+		}
 
 		objc_autoreleasePoolPop(pool);
 	}
+
+	/*
+	 * As long as we have data in the read buffer for any stream, we don't
+	 * want to block.
+	 */
+	return foundInReadBuffer;
 }
 @end

src/OFKernelEventObserver_LockedQueue.m
@@ -170,6 +170,8 @@ enum {
 
 - (void)OF_processQueue
 {
+	void *pool = objc_autoreleasePoolPush();
+
 #ifdef OF_HAVE_THREADS
 	[_mutex lock];
 	@try {
@@ -229,5 +231,7 @@ enum {
 		[_mutex unlock];
 	}
 #endif
+
+	objc_autoreleasePoolPop(pool);
 }
 @end

src/OFKernelEventObserver_epoll.m
@@ -209,13 +209,11 @@ static const of_map_table_functions_t mapFunctions = { NULL };
 - (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
 	OFNull *nullObject = [OFNull null];
-	void *pool = objc_autoreleasePoolPush();
 	struct epoll_event eventList[EVENTLIST_SIZE];
 	int events;
 
-	[self OF_processReadBuffers];
-
-	objc_autoreleasePoolPop(pool);
+	if ([self OF_processReadBuffers])
+		return;
 
 	events = epoll_wait(_epfd, eventList, EVENTLIST_SIZE,
 	    (timeInterval != -1 ? timeInterval * 1000 : -1));

src/OFKernelEventObserver_kqueue.m
@@ -205,17 +205,15 @@
 
 - (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
-	void *pool = objc_autoreleasePoolPush();
 	struct timespec timeout;
 	struct kevent eventList[EVENTLIST_SIZE];
 	int events;
 
+	if ([self OF_processReadBuffers])
+		return;
+
 	timeout.tv_sec = (time_t)timeInterval;
 	timeout.tv_nsec = lrint((timeInterval - timeout.tv_sec) * 1000000000);
-
-	[self OF_processReadBuffers];
-
-	objc_autoreleasePoolPop(pool);
 
 	events = kevent(_kernelQueue, NULL, 0, eventList, EVENTLIST_SIZE,
 	    (timeInterval != -1 ? &timeout : NULL));
@@ -225,6 +223,8 @@
 								 errNo: errno];
 
 	for (int i = 0; i < events; i++) {
+		void *pool;
+
 		if (eventList[i].flags & EV_ERROR)
 			@throw [OFObserveFailedException
 			    exceptionWithObserver: self

src/OFKernelEventObserver_poll.m
@@ -159,15 +159,14 @@
 
 - (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
-	void *pool = objc_autoreleasePoolPush();
 	struct pollfd *FDs;
 	int events;
 	size_t nFDs;
 
 	[self OF_processQueue];
-	[self OF_processReadBuffers];
 
-	objc_autoreleasePoolPop(pool);
+	if ([self OF_processReadBuffers])
+		return;
 
 	FDs = [_FDs items];
 	nFDs = [_FDs count];
@@ -188,6 +187,8 @@
 		assert(FDs[i].fd <= _maxFD);
 
 		if (FDs[i].revents & POLLIN) {
+			void *pool;
+
 			if (FDs[i].fd == _cancelFD[0]) {
 				char buffer;
 
@@ -213,7 +214,7 @@
 		}
 
 		if (FDs[i].revents & POLLOUT) {
-			pool = objc_autoreleasePoolPush();
+			void *pool = objc_autoreleasePoolPush();
 
 			if ([_delegate respondsToSelector:
 			    @selector(objectIsReadyForWriting:)])

src/OFKernelEventObserver_select.m
@@ -132,7 +132,6 @@
 
 - (void)observeForTimeInterval: (of_time_interval_t)timeInterval
 {
-	void *pool = objc_autoreleasePoolPush();
 	id const *objects;
 	fd_set readFDs;
 	fd_set writeFDs;
@@ -141,9 +140,9 @@
 	size_t count;
 
 	[self OF_processQueue];
-	[self OF_processReadBuffers];
 
-	objc_autoreleasePoolPop(pool);
+	if ([self OF_processReadBuffers])
+		return;
 
 #ifdef FD_COPY
 	FD_COPY(&_readFDs, &readFDs);
@@ -188,10 +187,8 @@
 	count = [_readObjects count];
 
 	for (size_t i = 0; i < count; i++) {
-		int fd;
-
-		pool = objc_autoreleasePoolPush();
-		fd = [objects[i] fileDescriptorForReading];
+		void *pool = objc_autoreleasePoolPush();
+		int fd = [objects[i] fileDescriptorForReading];
 
 		if (FD_ISSET(fd, &readFDs) && [_delegate respondsToSelector:
 		    @selector(objectIsReadyForReading:)])
@@ -204,10 +201,8 @@
 	count = [_writeObjects count];
 
 	for (size_t i = 0; i < count; i++) {
-		int fd;
-
-		pool = objc_autoreleasePoolPush();
-		fd = [objects[i] fileDescriptorForWriting];
+		void *pool = objc_autoreleasePoolPush();
+		int fd = [objects[i] fileDescriptorForWriting];
 
 		if (FD_ISSET(fd, &writeFDs) && [_delegate respondsToSelector:
 		    @selector(objectIsReadyForWriting:)])