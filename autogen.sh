21de769a76208ff3e2dd44517f46ac76f15fcbac
Jonathan Schleifer <js@heap.zone>
2017-09-10 23:04:26 +0200

    Set AUTO{CONF,MAKE}_VERSION for OpenBSD

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
+
+# Set a version for OpenBSD
+: ${AUTOCONF_VERSION:=2.69}
+: ${AUTOMAKE_VERSION:=1.15}
+export AUTOCONF_VERSION AUTOMAKE_VERSION
+
 aclocal -I build-aux/m4
 autoconf
 autoheader
