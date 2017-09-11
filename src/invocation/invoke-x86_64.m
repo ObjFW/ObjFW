b4a724e2fc6990642a4514fbb20b90c69951e4c1
Jonathan Schleifer <js@heap.zone>
2017-09-11 17:37:05 +0200

    invoke-x86_64.m: Support passing args via stack

--- src/invocation/invoke-x86_64.m
+++ src/invocation/invoke-x86_64.m
@@ -17,36 +17,41 @@
 #include "config.h"
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <xmmintrin.h>
 
 #import "OFInvocation.h"
 #import "OFMethodSignature.h"
 
 #import "OFInvalidFormatException.h"
+#import "OFOutOfMemoryException.h"
 
 #define NUM_GPR_IN 6
 #define NUM_GPR_OUT 2
 #define NUM_SSE_IN 8
 #define NUM_SSE_OUT 2
 
-struct registers {
+struct call_context {
 	uint64_t gpr[NUM_GPR_IN + NUM_GPR_OUT];
 	__m128 sse[NUM_SSE_IN];
-	uint8_t num_sse;
+	uint8_t num_sse_used;
+	uint64_t stack_size;
+	uint64_t stack[];
 };
 
-extern void of_invocation_call(struct registers *);
+extern void of_invocation_call(struct call_context *);
 
 void
 of_invocation_invoke(OFInvocation *invocation)
 {
 	OFMethodSignature *methodSignature = [invocation methodSignature];
 	size_t numberOfArguments = [methodSignature numberOfArguments];
+	struct call_context *context;
 	const char *typeEncoding;
-	struct registers registers;
 	size_t currentGPR = 0, currentSSE = 0;
 
-	memset(&registers, '\0', sizeof(registers));
+	if ((context = calloc(sizeof(*context), 1)) == NULL)
+		@throw [OFOutOfMemoryException exception];
 
 	for (size_t i = 0; i < numberOfArguments; i++) {
 		union {
@@ -120,27 +125,62 @@ of_invocation_invoke(OFInvocation *invocation)
 		/* TODO: 'j' */
 #endif
 		default:
+			free(context);
 			@throw [OFInvalidFormatException exception];
 #undef CASE_GPR
 		}
 
 		if (valueType == VALUE_GPR) {
 			if (currentGPR < NUM_GPR_IN)
-				registers.gpr[currentGPR++] = value.gpr;
-			else
-				/* TODO */
-				abort();
+				context->gpr[currentGPR++] = value.gpr;
+			else {
+				struct call_context *newContext;
+
+				context->stack_size++;
+
+				newContext = realloc(context,
+				    sizeof(*context) + context->stack_size * 8);
+				if (newContext == NULL) {
+					free(context);
+					@throw [OFOutOfMemoryException
+					    exceptionWithRequestedSize:
+					    sizeof(*context) +
+					    context->stack_size * 8];
+				}
+
+				context = newContext;
+				context->stack[context->stack_size - 1] =
+				    value.gpr;
+			}
 		} else if (valueType == VALUE_SSE) {
 			if (currentSSE < NUM_SSE_IN) {
-				registers.sse[currentSSE++] = value.sse;
-				registers.num_sse++;
-			} else
-				/* TODO */
-				abort();
+				context->sse[currentSSE++] = value.sse;
+				context->num_sse_used++;
+			} else {
+				struct call_context *newContext;
+				double tmp;
+
+				context->stack_size++;
+
+				newContext = realloc(context,
+				    sizeof(*context) + context->stack_size * 8);
+				if (newContext == NULL) {
+					free(context);
+					@throw [OFOutOfMemoryException
+					    exceptionWithRequestedSize:
+					    sizeof(*context) +
+					    context->stack_size * 8];
+				}
+
+				context = newContext;
+				_mm_store_sd(&tmp, value.sse);
+				memcpy(&context->stack[context->stack_size - 1],
+				    &tmp, 8);
+			}
 		}
 	}
 
-	of_invocation_call(&registers);
+	of_invocation_call(context);
 
 	typeEncoding = [methodSignature methodReturnType];
 
@@ -148,12 +188,12 @@ of_invocation_invoke(OFInvocation *invocation)
 		typeEncoding++;
 
 	switch (*typeEncoding) {
-#define CASE_GPR(encoding, type)					    \
-		case encoding:						    \
-			{						    \
-				type tmp = (type)registers.gpr[NUM_GPR_IN]; \
-				[invocation setReturnValue: &tmp];	    \
-			}						    \
+#define CASE_GPR(encoding, type)					   \
+		case encoding:						   \
+			{						   \
+				type tmp = (type)context->gpr[NUM_GPR_IN]; \
+				[invocation setReturnValue: &tmp];	   \
+			}						   \
 			break;
 		CASE_GPR('c', char)
 		CASE_GPR('C', unsigned char)
@@ -172,14 +212,14 @@ of_invocation_invoke(OFInvocation *invocation)
 		case 'f':
 			{
 				float tmp;
-				_mm_store_ss(&tmp, registers.sse[0]);
+				_mm_store_ss(&tmp, context->sse[0]);
 				[invocation setReturnValue: &tmp];
 			}
 			break;
 		case 'd':
 			{
 				double tmp;
-				_mm_store_sd(&tmp, registers.sse[0]);
+				_mm_store_sd(&tmp, context->sse[0]);
 				[invocation setReturnValue: &tmp];
 			}
 			break;
@@ -197,7 +237,10 @@ of_invocation_invoke(OFInvocation *invocation)
 		/* TODO: 'j' */
 #endif
 		default:
+			free(context);
 			@throw [OFInvalidFormatException exception];
 #undef CASE_GPR
 	}
+
+	free(context);
 }
--- tests/OFInvocationTests.m
+++ tests/OFInvocationTests.m
@@ -32,19 +32,61 @@ struct test_struct {
 };
 
 @implementation TestsAppDelegate (OFInvocationTests)
-- (struct test_struct)invocationTestMethod: (unsigned char)c
-					  : (unsigned int)i
-					  : (struct test_struct *)ptr
-					  : (struct test_struct)st
+- (struct test_struct)invocationTestMethod1: (unsigned char)c
+					   : (unsigned int)i
+					   : (struct test_struct *)ptr
+					   : (struct test_struct)st
 {
 	return st;
+}
+
+- (int)invocationTestMethod2: (int)i1
+			    : (int)i2
+			    : (int)i3
+			    : (int)i4
+			    : (int)i5
+			    : (int)i6
+			    : (int)i7
+			    : (int)i8
+			    : (int)i9
+			    : (int)i10
+			    : (int)i11
+			    : (int)i12
+			    : (int)i13
+			    : (int)i14
+			    : (int)i15
+			    : (int)i16
+{
+	return (i1 + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9 + i10 + i11 +
+	    i12 + i13 + i14 + i15 + i16) / 16;
+}
+
+- (double)invocationTestMethod3: (double)d1
+			       : (double)d2
+			       : (double)d3
+			       : (double)d4
+			       : (double)d5
+			       : (double)d6
+			       : (double)d7
+			       : (double)d8
+			       : (double)d9
+			       : (double)d10
+			       : (double)d11
+			       : (double)d12
+			       : (double)d13
+			       : (double)d14
+			       : (double)d15
+			       : (double)d16
+{
+	return (d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 +
+	    d12 + d13 + d14 + d15 + d16) / 16;
 }
 
 - (void)invocationTests
 {
 	OFAutoreleasePool *pool = [[OFAutoreleasePool alloc] init];
-	OFMethodSignature *sig = [self methodSignatureForSelector:
-	    @selector(invocationTestMethod::::)];
+	SEL selector = @selector(invocationTestMethod1::::);
+	OFMethodSignature *sig = [self methodSignatureForSelector: selector];
 	OFInvocation *invocation;
 	struct test_struct st, st2, *stp = &st, *stp2;
 	unsigned const char c = 0xAA;
@@ -93,6 +135,47 @@ struct test_struct {
 	TEST(@"-[getArgument:atIndex:] #4", R([invocation getArgument: &st2
 							      atIndex: 5]) &&
 	    memcmp(&st, &st2, sizeof(st)) == 0)
+
+#ifdef OF_INVOCATION_CAN_INVOKE
+	/* -[invoke] #1 */
+	selector = @selector(invocationTestMethod2::::::::::::::::);
+	invocation = [OFInvocation invocationWithMethodSignature:
+	    [self methodSignatureForSelector: selector]];
+
+	[invocation setArgument: &self
+			atIndex: 0];
+	[invocation setArgument: &selector
+			atIndex: 1];
+
+	for (int i = 1; i <= 16; i++)
+		[invocation setArgument: &i
+				atIndex: i + 1];
+
+	int intResult;
+	TEST(@"-[invoke] #1", R([invocation invoke]) &&
+	    R([invocation getReturnValue: &intResult]) && intResult == 8)
+
+	/* -[invoke] #2 */
+	selector = @selector(invocationTestMethod3::::::::::::::::);
+	invocation = [OFInvocation invocationWithMethodSignature:
+	    [self methodSignatureForSelector: selector]];
+
+	[invocation setArgument: &self
+			atIndex: 0];
+	[invocation setArgument: &selector
+			atIndex: 1];
+
+	for (int i = 1; i <= 16; i++) {
+		double d = i;
+		[invocation setArgument: &d
+				atIndex: i + 1];
+	}
+
+	double doubleResult;
+	TEST(@"-[invoke] #2", R([invocation invoke]) &&
+	    R([invocation getReturnValue: &doubleResult]) &&
+	    doubleResult == 8.5)
+#endif
 
 	[pool drain];
 }
