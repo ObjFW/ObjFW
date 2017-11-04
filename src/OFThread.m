dc06724cb2c43225f72bbcd92e5dad2fd8e0d36b
Jonathan Schleifer <js@heap.zone>
2017-11-04 21:56:43 +0100

    +[OFThread threadDictionary]: Handle nil thread

--- src/OFThread.m
+++ src/OFThread.m
@@ -176,6 +176,9 @@ callMain(id object)
 {
 	OFThread *thread = of_tlskey_get(threadSelfKey);
 
+	if (thread == nil)
+		return nil;
+
 	if (thread->_threadDictionary == nil)
 		thread->_threadDictionary = [[OFMutableDictionary alloc] init];
 
