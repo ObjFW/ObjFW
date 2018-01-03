6bb6b19385aa87f623b27c4c54b42c8be867b739
Jonathan Schleifer <js@heap.zone>
2018-01-03 20:49:46 +0100

    Update copyright notice

--- generators/copyright.h
+++ generators/copyright.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017
+ * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017,
+ *               2018
  *   Jonathan Schleifer <js@heap.zone>
  *
  * All rights reserved.
@@ -19,7 +20,8 @@
 #define COPYRIGHT \
     @"/*\n"								       \
     @" * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, " \
-    @"2017\n"								       \
+    @"2017,\n"								       \
+    @" *               2018,\n"						       \
     @" *   Jonathan Schleifer <js@heap.zone>\n"				       \
     @" *\n"								       \
     @" * All rights reserved.\n"					       \
