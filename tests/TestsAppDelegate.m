6ca9a94dc3c8f379fe612260a79090ccbf47dd8a
Jonathan Schleifer <js@heap.zone>
2018-08-11 16:34:39 +0200

    Fix compilation with --disable-sockets

--- tests/TestsAppDelegate.m
+++ tests/TestsAppDelegate.m
@@ -420,8 +420,10 @@ main(int argc, char *argv[])
 #if defined(OF_HAVE_SOCKETS) && defined(OF_HAVE_THREADS)
 	[self HTTPClientTests];
 #endif
+#ifdef OF_HAVE_SOCKETS
 	[self HTTPCookieTests];
 	[self HTTPCookieManagerTests];
+#endif
 	[self XMLParserTests];
 	[self XMLNodeTests];
 	[self XMLElementBuilderTests];
