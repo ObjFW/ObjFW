9d91ae96f9a8286d3789ff292fff3ca3911a81b8
Jonathan Schleifer <js@heap.zone>
2018-11-04 14:29:14 +0100

    autogen.sh: Use automake 1.16 on OpenBSD

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@ set -e
 
 # Set a version for OpenBSD
 : ${AUTOCONF_VERSION:=2.69}
-: ${AUTOMAKE_VERSION:=1.15}
+: ${AUTOMAKE_VERSION:=1.16}
 export AUTOCONF_VERSION AUTOMAKE_VERSION
 
 aclocal -I build-aux/m4
