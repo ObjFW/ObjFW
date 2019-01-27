94072bcaac2602d9e3fea1467e3ad0254c93c2d3
Jonathan Schleifer <js@heap.zone>
2019-01-27 17:13:12 +0100

    atomic.h: Don't use assembly implementation on AIX
    The assembler on AIX is too limited.

--- src/atomic.h
+++ src/atomic.h
@@ -27,7 +27,7 @@
 # import "atomic_no_threads.h"
 #elif defined(OF_X86_64_ASM) || defined(OF_X86_ASM)
 # import "atomic_x86.h"
-#elif defined(OF_POWERPC_ASM) && !defined(__APPLE_CC__)
+#elif defined(OF_POWERPC_ASM) && !defined(__APPLE_CC__) && !defined(OF_AIX)
 # import "atomic_powerpc.h"
 #elif defined(OF_HAVE_ATOMIC_BUILTINS)
 # import "atomic_builtins.h"
