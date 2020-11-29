2a3e362306eea01f867046433feb35f4d6d207b4
Jonathan Schleifer <js@nil.im>
2020-11-29 02:21:31 +0000

    Fix a few places using msec instead of usec
    FossilOrigin-Name: 208285040f66ffc8b8b4341f27561e84b62bb2c6c5816838d2168848cb4cd36c

--- src/OFSelectKernelEventObserver.m
+++ src/OFSelectKernelEventObserver.m
@@ -198,7 +198,7 @@
 #else
 	timeout.tv_sec = (long)timeInterval;
 #endif
-	timeout.tv_usec = (int)((timeInterval - timeout.tv_sec) * 1000);
+	timeout.tv_usec = (int)((timeInterval - timeout.tv_sec) * 1000000);
 
 #ifdef OF_AMIGAOS
 	if ((cancelSignal = AllocSignal(-1)) == (ULONG)-1)
