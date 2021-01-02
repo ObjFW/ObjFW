5373e218f9c6c175e62d1d3411a6ccd0ba70ada0
Jonathan Schleifer <js@nil.im>
2021-01-02 22:04:26 +0000

    Update copyright
    FossilOrigin-Name: 374e1a1bfa30d76718992164f05f46f572bf8a5fe998b48a70ad746b6ff7ab81

--- generators/library/copyright.h
+++ generators/library/copyright.h
@@ -1,7 +1,5 @@
 /*
- * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017,
- *               2018, 2019, 2020
- *   Jonathan Schleifer <js@nil.im>
+ * Copyright (c) 2008-2021 Jonathan Schleifer <js@nil.im>
  *
  * All rights reserved.
  *
@@ -19,10 +17,7 @@
 
 #define COPYRIGHT \
     @"/*\n"								       \
-    @" * Copyright (c) 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, " \
-    @"2017,\n"								       \
-    @" *               2018, 2019, 2020\n"				       \
-    @" *   Jonathan Schleifer <js@nil.im>\n"				       \
+    @" * Copyright (c) 2008-2021 Jonathan Schleifer <js@nil.im>\n"	       \
     @" *\n"								       \
     @" * All rights reserved.\n"					       \
     @" *\n"								       \
