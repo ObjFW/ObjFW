262cfa8688d736fae7ead02a9ba52f82990d7876
Jonathan Schleifer <js@nil.im>
2021-05-13 20:07:50 +0000

    tests: Use +[OFSystem temporaryDirectoryPath]
    FossilOrigin-Name: b30979ffc9910abb38df2086d552cb291b1f6eb6474ec335088121f74360f092

--- tests/OFINIFileTests.m
+++ tests/OFINIFileTests.m
@@ -114,19 +114,13 @@ static OFString *module;
 
 	/* FIXME: Find a way to write files on Nintendo DS */
 #ifndef OF_NINTENDO_DS
-# ifndef OF_IOS
-	writePath = @"tmpfile.ini";
-# else
-	writePath = [OFString pathWithComponents: [OFArray arrayWithObjects:
-	    [[OFApplication environment] objectForKey: @"HOME"],
-	    @"tmp", @"tmpfile.ini", nil]];
-# endif
+	writePath = [[OFSystemInfo temporaryDirectoryPath]
+	    stringByAppendingPathComponent: @"objfw-tests.ini"];
 	TEST(@"-[writeToFile:encoding:]",
 	    R([file writeToFile: writePath
 		       encoding: OFStringEncodingCodepage437]) &&
-	    [[OFString
-		stringWithContentsOfFile: writePath
-				encoding: OFStringEncodingCodepage437]
+	    [[OFString stringWithContentsOfFile: writePath
+				       encoding: OFStringEncodingCodepage437]
 	    isEqual: output])
 	[[OFFileManager defaultManager] removeItemAtPath: writePath];
 #else
