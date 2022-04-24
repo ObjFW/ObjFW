76e515c391d550d052d5b2d81c18b8203e4a96fb
Jonathan Schleifier <js@nil.im>
2022-04-24 09:40:50 +0000

    Reorder more declarations for consistency
    FossilOrigin-Name: 7e1d2b5cd60b2a7032744bdbcea079549407fcf5718cc07b0542670c9abb394b

--- src/exceptions/OFRetrieveItemAttributesFailedException.h
+++ src/exceptions/OFRetrieveItemAttributesFailedException.h
@@ -42,8 +42,6 @@ OF_ASSUME_NONNULL_BEGIN
  */
 @property (readonly, nonatomic) int errNo;
 
-+ (instancetype)exception OF_UNAVAILABLE;
-
 /**
  * @brief Creates a new, autoreleased retrieve item attributes failed exception.
  *
@@ -53,7 +51,7 @@ OF_ASSUME_NONNULL_BEGIN
  */
 + (instancetype)exceptionWithURL: (OFURL *)URL errNo: (int)errNo;
 
-- (instancetype)init OF_UNAVAILABLE;
++ (instancetype)exception OF_UNAVAILABLE;
 
 /**
  * @brief Initializes an already allocated retrieve item attributes failed
@@ -65,6 +63,8 @@ OF_ASSUME_NONNULL_BEGIN
  */
 - (instancetype)initWithURL: (OFURL *)URL
 		      errNo: (int)errNo OF_DESIGNATED_INITIALIZER;
+
+- (instancetype)init OF_UNAVAILABLE;
 @end
 
 OF_ASSUME_NONNULL_END
