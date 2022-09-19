59bbf86e61a51c4c10adf1762c82eed694b64b62
Jonathan Schleifer <js@nil.im>
2022-09-19 19:41:39 +0000

    Fix a few documentation issues
    FossilOrigin-Name: 97f3f7b6480414e523d42ae3ec771091c1cca1b5dd27311d72592ddcbcdb084f

--- src/OFKernelEventObserver.h
+++ src/OFKernelEventObserver.h
@@ -45,8 +45,8 @@ OF_ASSUME_NONNULL_BEGIN
  * @brief This callback is called when an object did get ready for reading.
  *
  * @note If the object is a subclass of @ref OFStream and
- *	 @ref OFStream::tryReadLine or @ref OFStream::tryReadTillDelimiter: has
- *	 been called on the stream, this callback will not be called again
+ *	 @ref OFStream::tryReadLine or @ref OFStream::tryReadUntilDelimiter:
+ *	 has been called on the stream, this callback will not be called again
  *	 until new data has been received, even though there is still data in
  *	 the cache. The reason for this is to prevent spinning in a loop when
  *	 there is an incomplete string in the cache. Once the string has been
