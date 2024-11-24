76dbcb0ecfd721685d271870e751ba0c9f20a7e6
Jonathan Schleifer <js@nil.im>
2024-11-24 21:28:39 +0000

    Use SSLCreateContext if available
    It is required to use it on iOS.
    
    FossilOrigin-Name: 867724b05f45a274645c27649f57547ef034e1531643300f0aaca9c894a4340c

--- src/tls/OFSecureTransportTLSStream.m
+++ src/tls/OFSecureTransportTLSStream.m
@@ -143,7 +143,11 @@ writeFunc(SSLConnectionRef connection, const void *data, size_t *dataLength)
 	_host = nil;
 
 	SSLClose(_context);
+#ifdef HAVE_SSLCREATECONTEXT
+	CFRelease(_context);
+#else
 	SSLDisposeContext(_context);
+#endif
 	_context = NULL;
 
 	[super close];
@@ -208,7 +212,13 @@ writeFunc(SSLConnectionRef connection, const void *data, size_t *dataLength)
 	if (_context != NULL)
 		@throw [OFAlreadyOpenException exceptionWithObject: self];
 
+#ifdef HAVE_SSLCREATECONTEXT
+	if ((_context = SSLCreateContext(kCFAllocatorDefault,
+	    (server ? kSSLServerSide : kSSLClientSide),
+	    kSSLStreamType)) == NULL)
+#else
 	if (SSLNewContext(server, &_context) != noErr)
+#endif
 		@throw [OFTLSHandshakeFailedException
 		    exceptionWithStream: self
 				   host: host
