0afb7c5da2a508e7b08b6066888b2245d9b0e955
Jonathan Schleifer <js@nil.im>
2024-12-20 22:41:24 +0000

    ObjFWBridge: Add support for bridging dates
    FossilOrigin-Name: 3fd610018a86b148f5933103c7e7447ef172eb59231efeea90052ab3daca2559

---
+++ src/bridge/NSDate+OFObject.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2008-2024 Jonathan Schleifer <js@nil.im>
+ *
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3.0 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * version 3.0 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3.0 along with this program. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#import <Foundation/NSDate.h>
+
+#import "NSBridging.h"
+
+OF_ASSUME_NONNULL_BEGIN
+
+@class OFDate;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern int _NSDate_OFObject_reference OF_VISIBILITY_HIDDEN;
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @category NSDate (OFObject)
+ *	     NSDate+OFObject.h ObjFWBridge/NSDate+OFObject.h
+ *
+ * @brief Support for bridging NSDates to OFDates.
+ */
+@interface NSDate (OFObject) <NSBridging>
+@property (readonly, nonatomic) OFDate *OFObject;
+@end
+
+OF_ASSUME_NONNULL_END
---
+++ src/bridge/OFDate+NSObject.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2008-2024 Jonathan Schleifer <js@nil.im>
+ *
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3.0 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * version 3.0 for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3.0 along with this program. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#ifdef OBJFWBRIDGE_LOCAL_INCLUDES
+# import "OFDate.h"
+#else
+# if defined(__has_feature) && __has_feature(modules)
+@import ObjFW;
+# else
+#  import <ObjFW/OFDate.h>
+# endif
+#endif
+
+#import "OFBridging.h"
+
+OF_ASSUME_NONNULL_BEGIN
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern int _OFDate_NSObject_reference OF_VISIBILITY_HIDDEN;
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @category OFDate (NSObject) \
+ *	     OFDate+NSObject.h ObjFWBridge/OFDate+NSObject.h
+ *
+ * @brief Support for bridging OFDates to NSDates.
+ */
+@interface OFDate (NSObject) <OFBridging>
+@property (readonly, nonatomic) NSDate *NSObject;
+@end
+
+OF_ASSUME_NONNULL_END
