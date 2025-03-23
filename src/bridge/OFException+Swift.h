bc3c74822161a75cbd7bf40ff1a17d85832632a5
Jonathan Schleifer <js@nil.im>
2025-03-23 00:32:06 +0000

    Small documentation fix
    FossilOrigin-Name: fe3184f8d83f95caf13d3aca2b6bf91c38f3436edc12a2ab429f7f37c2a0a961

--- src/bridge/OFException+Swift.h
+++ src/bridge/OFException+Swift.h
@@ -32,7 +32,7 @@ OF_ASSUME_NONNULL_BEGIN
 /**
  * @category OFException (Swift) OFException+Swift.h ObjFWBridge/ObjFWBridge.h
  *
- * @brief Support for using throwing and catching an @ref OFException in Swift.
+ * @brief Support for throwing and catching an @ref OFException in Swift.
  */
 @interface OFException (Swift)
 #ifdef OF_HAVE_BLOCKS
