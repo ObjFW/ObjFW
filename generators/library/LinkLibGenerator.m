0be444e7a3fd8e9c5f3bc4507471b77523fe530a
Jonathan Schleifer <js@nil.im>
2025-06-27 23:47:57 +0200

    generators/library: Use ARC functions for RR

--- generators/library/LinkLibGenerator.m
+++ generators/library/LinkLibGenerator.m
@@ -49,8 +49,8 @@
 			@throw [OFUnsupportedVersionException
 			    exceptionWithVersion: version.stringValue];
 
-		_library = [library retain];
-		_impl = [impl retain];
+		_library = objc_retain(library);
+		_impl = objc_retain(impl);
 	} @catch (id e) {
 		[self release];
 		@throw e;
@@ -61,8 +61,8 @@
 
 - (void)dealloc
 {
-	[_library release];
-	[_impl release];
+	objc_release(_library);
+	objc_release(_impl);
 
 	[super dealloc];
 }
