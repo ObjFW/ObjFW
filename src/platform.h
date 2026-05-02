158cbc910b55335c8c4aa5ba085f873a8818ac0d
Jonathan Schleifer <js@nil.im>
2026-05-02 03:15:44 +0200

    Fix OF_AMD64 define on Windows

--- src/platform.h
+++ src/platform.h
@@ -29,7 +29,8 @@
 # endif
 #endif
 
-#if (defined(__x86_64__) || defined(__amd64__)) && defined(__LP64__)
+#if (defined(__x86_64__) || defined(__amd64__)) && \
+    (defined(__LP64__) || defined(_WIN64))
 # define OF_AMD64
 #elif defined(__i386__)
 # define OF_X86
