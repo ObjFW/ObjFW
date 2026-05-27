a688518851ec33ebc9226ec682af7a2784e3c388
Jonathan Schleifer <js@nil.im>
2026-05-27 22:01:35 +0200

    OFMutableString: Fix wrong variable
    This had no effect in practice as both variables currently have the same
    value.
    
    (cherry picked from commit 91e4e39f0ef416e59e6e1116954e46d22eb9f561)

--- src/OFMutableString.m
+++ src/OFMutableString.m
@@ -237,7 +237,7 @@ OF_SINGLETON_METHODS
 
 		if (isStart) {
 			table = startTable;
-			tableSize = middleTableSize;
+			tableSize = startTableSize;
 		} else {
 			table = middleTable;
 			tableSize = middleTableSize;
