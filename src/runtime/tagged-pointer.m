42547fc5d48f61c4de920d7c28354fe8bda3495e
Jonathan Schleifer <js@nil.im>
2026-08-10 22:04:34 +0200

    runtime: Fix missing OF_VISIBILITY_INTERNAL

--- src/runtime/tagged-pointer.m
+++ src/runtime/tagged-pointer.m
@@ -26,9 +26,10 @@
 #define numTaggedPointerBits 4
 #define maxNumTaggedPointerClasses (1 << (numTaggedPointerBits - 1))
 
-Class _objc_taggedPointerClasses[maxNumTaggedPointerClasses];
+Class _objc_taggedPointerClasses[maxNumTaggedPointerClasses]
+    OF_VISIBILITY_INTERNAL;
 static int taggedPointerClassesCount;
-uintptr_t _objc_taggedPointerSecret;
+uintptr_t _objc_taggedPointerSecret OF_VISIBILITY_INTERNAL;
 
 void
 objc_setTaggedPointerSecret(uintptr_t secret)
