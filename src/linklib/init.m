67a7627fdc7caf49633b218f02f8c6c02fdbd13c
Jonathan Schleifer <js@nil.im>
2026-09-18 22:06:32 +0200

    Add OFASN1TextString to .library linklib

--- src/linklib/init.m
+++ src/linklib/init.m
@@ -68,6 +68,7 @@ void *__objc_class_name_OFASN1OctetString;
 void *__objc_class_name_OFASN1PrintableString;
 void *__objc_class_name_OFASN1Sequence;
 void *__objc_class_name_OFASN1Set;
+void *__objc_class_name_OFASN1TextString;
 void *__objc_class_name_OFASN1UTF8String;
 void *__objc_class_name_OFASN1Value;
 void *__objc_class_name_OFActivateSandboxFailedException;
