213414d5e443df41111140bcb948116e463b06f1
Jonathan Schleifer <js@nil.im>
2026-09-19 23:17:46 +0200

    OFX509Certificate: Add support for parsing PEM

--- src/OFX509Certificate.m
+++ src/OFX509Certificate.m
@@ -21,7 +21,13 @@
 
 #import "OFX509Certificate.h"
 #import "OFASN1Sequence.h"
+#import "OFArray.h"
+#import "OFData.h"
+#import "OFIRIHandler.h"
+#import "OFPair.h"
+#import "OFStream.h"
 
+#import "OFInvalidArgumentException.h"
 #import "OFInvalidFormatException.h"
 #import "OFNotImplementedException.h"
 
@@ -38,11 +44,106 @@
 	return false;
 }
 
+static void
+parsePEM(OFStream *stream, void (*callback)(OFString *, OFData *, void *ctx),
+    void *ctx)
+{
+	OFString *section = nil;
+	OFMutableString *string = nil;
+
+	OFString *line;
+	while ((line = [stream readLine]) != nil) {
+		if (section != nil) {
+			if ([line hasPrefix: @"-----END "]) {
+				if (![line hasSuffix: @"-----"])
+					@throw [OFInvalidFormatException
+					    exception];
+
+				if (![[line substringWithRange:
+				    OFMakeRange(9, line.length - 9 - 5)]
+				    isEqual: section])
+					@throw [OFInvalidFormatException
+					    exception];
+
+				OFData *data = [OFData
+				    dataWithBase64EncodedString: string];
+				callback(section, data, ctx);
+
+				section = nil;
+				string = nil;
+
+				continue;
+			}
+
+			[string appendString: line];
+		} else {
+			if ([line hasPrefix: @"-----BEGIN "]) {
+				if (![line hasSuffix: @"-----"])
+					@throw [OFInvalidFormatException
+					    exception];
+
+				section = [line substringWithRange:
+				    OFMakeRange(11, line.length - 11 - 5)];
+				string = [OFMutableString string];
+			}
+		}
+	}
+
+	if (section != nil)
+		@throw [OFInvalidFormatException exception];
+}
+
+static void
+parsePrivateKeyCallback(OFString *section, OFData *data, void *ctx)
+{
+	if (![section isEqual: @"PRIVATE KEY"])
+		@throw [OFInvalidArgumentException exception];
+
+	OF_KINDOF(OFASN1Value *) *privateKey = ctx;
+	*privateKey = data.valueByParsingDER;
+}
+
+static void
+parseCertificates(OFString *section, OFData *data, void *ctx)
+{
+	if (![section isEqual: @"CERTIFICATE"])
+		@throw [OFInvalidArgumentException exception];
+
+	OFPair *pair = ctx;
+	OFMutableArray *certificateChain = pair.firstObject;
+	OF_KINDOF(OFASN1Value *) privateKeyASN1Value;
+
+	if (certificateChain.count == 0)
+		privateKeyASN1Value = pair.secondObject;
+	else
+		privateKeyASN1Value = nil;
+
+	[certificateChain addObject:
+	    [OFX509Certificate certificateWithASN1Value: data.valueByParsingDER
+				    privateKeyASN1Value: privateKeyASN1Value]];
+}
+
 + (OFArray OF_GENERIC(OFX509Certificate *) *)
     certificateChainFromPEMFileAtIRI: (OFIRI *)certificatesIRI
 		       privateKeyIRI: (OFIRI *)privateKeyIRI
 {
-	OF_UNRECOGNIZED_SELECTOR
+	OFMutableArray *certificateChain = [OFMutableArray array];
+	void *pool = objc_autoreleasePoolPush();
+	OF_KINDOF(OFASN1Value *) privateKey = nil;
+
+	if (privateKeyIRI != nil)
+		parsePEM([OFIRIHandler openItemAtIRI: privateKeyIRI
+						mode: @"r"],
+		    parsePrivateKeyCallback, &privateKey);
+
+	parsePEM([OFIRIHandler openItemAtIRI: certificatesIRI
+					mode: @"r"],
+	    parseCertificates, [OFPair pairWithFirstObject: certificateChain
+					      secondObject: privateKey]);
+
+	objc_autoreleasePoolPop(pool);
+
+	return certificateChain;
 }
 
 + (OFArray OF_GENERIC(OFX509Certificate *) *)
